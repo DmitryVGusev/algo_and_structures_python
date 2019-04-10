52c2b2a0b6cb391714294bd88f4fa8ca688937f2
Dmitry <jinrlit@gmail.com>
2019-04-11 01:09:11 +0300

    Реализована задача 7

--- Lesson_2/7.py
+++ Lesson_2/7.py
@@ -3,3 +3,36 @@
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+
+
+def get_sum(n):
+    ans = 0
+    while n > 0:
+        ans += n
+        n -= 1
+    return ans
+
+
+def get_sum_recursive(n):
+    if n == 1:
+        return 1
+    else:
+        return n + get_sum_recursive(n - 1)
+
+
+if __name__ == '__main__':
+
+    # При смене этой переменной используется или не используется рекурсивная сумма
+    recursive = False
+    if recursive:
+        left_sum = get_sum_recursive
+    else:
+        left_sum = get_sum
+
+
+    n = 65
+    if left_sum(n) == (n * (n + 1) / 2):
+        print("Равенство выполняется")
+    else:
+        print("Равенсвто не выполняется")
+
