02629bedd84dec3f0824e8e413cd646d29f1e22a
Dmitry <jinrlit@gmail.com>
2019-04-11 02:09:14 +0300

    Реализована задача 5

--- Lesson_2/5.py
+++ Lesson_2/5.py
@@ -3,3 +3,28 @@
 под номером 32 и заканчивая 127-м включительно.
 Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 """
+
+
+def simbol_key_val(n):
+    return f"{n}-{chr(n)} "
+
+
+def get_simbol_table(first, last, col=5):
+    """Вывести на экран коды и символы таблицы ASCII"""
+    ans = ''
+    counter = 0
+    while first <= last:
+        ans += simbol_key_val(first)
+        first += 1
+        counter += 1
+
+        # Перенос строки при превышении количества колонок в строке
+        if counter == col:
+            ans += '\n'
+            counter = 0
+
+    return ans
+
+
+if __name__ == '__main__':
+    print(get_simbol_table(32, 127))
