5487277e1d6f2ca1c39aff82be8b4b09d78a583f
Dmitry <jinrlit@gmail.com>
2019-04-10 14:37:36 +0300

    работающий алгоритм шестого задания

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,13 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+
+def find_letter_position():
+    """Определяет положение буквы в алфафите"""
+    letter = input("Введите букву: ")
+    letter_pos = ord(letter) - ord('a') + 1
+
+    print(f"Буква '{letter}' находится в алфавите на {letter_pos} месте")
+
+
+if __name__ == '__main__':
+    find_letter_position()
