dac4fef7e36ac34736b032ec3752f057e6d041db
Dmitry <jinrlit@gmail.com>
2019-04-20 13:55:35 +0300

    реализованные но не оформленные алгоритмы

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,3 +3,29 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+
+
+def reverse_num(num):
+    """Возвращает заданное число в обратном порядке"""
+    reverse = ''
+    while num > 0:
+        reverse += str(num % 10)
+        num //= 10
+    return reverse
+
+
+def reverse_num_recursive(num, rev=''):
+    """Возвращает заданное число в обратном порядке"""
+    if num > 0:
+        rev += str(num % 10)
+        return reverse_num_recursive(num // 10, rev=rev)
+    else:
+        return rev
+
+
+if __name__ == '__main__':
+    import timeit
+    print(timeit.timeit("reverse_num(1234567)",
+                        setup="from __main__ import reverse_num"))
+    print(timeit.timeit("reverse_num_recursive(1234567)",
+                        setup="from __main__ import reverse_num_recursive"))
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -3,3 +3,59 @@
 Без использования «Решета Эратосфена»;
 Используя алгоритм «Решето Эратосфена»
 """
+import timeit
+
+
+def prime_list(num):
+    """
+    Выводит список простых чисел до указанного
+    :param num:
+    :return:
+    """
+    ans = [2]
+    for i in range(3, num+1):
+        if [x for x in range(2, i) if i % x == 0]:
+            continue
+        ans.append(i)
+    return ans
+
+
+def prime_list_2(num):
+    """
+    Выводит список простых чисел до указанного
+    :param num:
+    :return:
+    """
+    ans = [2]
+    for i in range(3, num+1):
+        if [x for x in range(2, i) if i % x == 0]:
+            continue
+        ans.append(i)
+    return ans
+
+
+def resheto(num):
+    """
+    Решето Эратосфена. Возвращает список простых чисел до введенного.
+    :param num:
+    :return:
+    """
+    ans = [x for x in range(2, num+1)]
+    for i in ans:
+        if i == 0:
+            continue
+        ind = 2*i - 2
+        while ind <= num -2:
+            ans[ind] = 0
+            ind += i
+    return [x for x in ans if x != 0]
+
+
+
+
+if __name__ == '__main__':
+    print(timeit.timeit("prime_list(100)", number=1000, setup="from __main__ import prime_list"))
+    print(timeit.timeit("resheto(100)", number=1000, setup="from __main__ import resheto"))
+    # print(prime_list(40))
+    # print(resheto(40))
+# print(timeit.timeit("gen_list(20)", setup="from __main__ import gen_list"))
