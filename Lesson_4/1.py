fd5509c6614f7bc6739165b10adeaf2ce3aaf679
Dmitry <jinrlit@gmail.com>
2019-04-20 16:50:55 +0300

    оформленные задачи

--- Lesson_4/1.py
+++ Lesson_4/1.py
@@ -3,29 +3,81 @@
 разработанных в рамках домашнего задания первых трех уроков.
 Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
 """
+import timeit
+import cProfile
+import sys
 
 
-def reverse_num(num):
-    """Возвращает заданное число в обратном порядке"""
-    reverse = ''
-    while num > 0:
-        reverse += str(num % 10)
-        num //= 10
-    return reverse
+"""
+Ниже представлены алгоритмы суммы n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
+Первый вариант церез цикл. Алгоритм линейный
+"""
 
 
-def reverse_num_recursive(num, rev=''):
-    """Возвращает заданное число в обратном порядке"""
-    if num > 0:
-        rev += str(num % 10)
-        return reverse_num_recursive(num // 10, rev=rev)
+def series_sum(n):
+    """Возвращает сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ..."""
+    summ = 1
+    el = 1
+    i = 1
+    while i < n:
+        el *= -0.5
+        summ += el
+        i += 1
+    return summ
+
+
+# 0.530сек - результат вызова функции 100000 раз
+print(timeit.timeit("series_sum(40)",
+                    setup="from __main__ import series_sum",
+                    number=100000))
+
+
+"""
+Второй вариант реализации алгоритма - через рекурсию.
+Алгоритм также линейный
+"""
+
+
+def series_sum_recursive(n, el=(-0.5), summ=1):
+    """Возвращает сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ..."""
+    if n == 1:
+        return summ
     else:
-        return rev
+        return summ + series_sum_recursive(n-1, el*(-0.5), el)
 
 
-if __name__ == '__main__':
-    import timeit
-    print(timeit.timeit("reverse_num(1234567)",
-                        setup="from __main__ import reverse_num"))
-    print(timeit.timeit("reverse_num_recursive(1234567)",
-                        setup="from __main__ import reverse_num_recursive"))
+# 1.197сек - результат вызова функции 100000 раз
+print(timeit.timeit("series_sum_recursive(40)",
+                    setup="from __main__ import series_sum_recursive",
+                    number=100000))
+
+
+"""
+Таким образом, решение через цикл показывает более чем в 2 раза прирост скорости исполнения задачи
+"""
+
+
+def main():
+    sys.setrecursionlimit(10100)
+    n = 10000
+    series_sum(n)
+    series_sum_recursive(n)
+
+
+cProfile.run('main()')
+
+
+"""
+         10006 function calls (7 primitive calls) in 0.020 seconds
+
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.001    0.001    0.001    0.001 1.py:17(series_sum)
+  10000/1    0.019    0.000    0.019    0.019 1.py:40(series_sum_recursive)
+        1    0.000    0.000    0.020    0.020 1.py:54(main)
+        1    0.000    0.000    0.020    0.020 <string>:1(<module>)
+        1    0.000    0.000    0.020    0.020 {built-in method builtins.exec}
+        1    0.000    0.000    0.000    0.000 {built-in method sys.setrecursionlimit}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+        
+Здесь же решение через цикл показывает прирост скорости примерно в 20 раз
+"""
--- Lesson_4/2.py
+++ Lesson_4/2.py
@@ -4,42 +4,58 @@
 Используя алгоритм «Решето Эратосфена»
 """
 import timeit
+import cProfile
+
+"""
+Первый вариант реализации поиска массива простых чисел:
+проверяем что сисло не делится на все числа до него
+Сложность алгоритма, как я понимаю O=n*(sum(0,N))
+"""
 
 
 def prime_list(num):
-    """
-    Выводит список простых чисел до указанного
-    :param num:
-    :return:
-    """
+    if num < 2:
+        return []
     ans = [2]
     for i in range(3, num+1):
         if [x for x in range(2, i) if i % x == 0]:
             continue
         ans.append(i)
     return ans
+
+
+# 0.424сек - результат вызова функции 1000 раз
+print(timeit.timeit("prime_list(100)", number=1000, setup="from __main__ import prime_list"))
+
+
+"""
+Второй вариант реализации поиска массива простых чисел:
+проверяем что число не делится на числа в диапозоне (2, корень числа)
+"""
 
 
 def prime_list_2(num):
-    """
-    Выводит список простых чисел до указанного
-    :param num:
-    :return:
-    """
+    if num < 2:
+        return []
     ans = [2]
     for i in range(3, num+1):
-        if [x for x in range(2, i) if i % x == 0]:
+        if [x for x in range(2, int(i**0.5) + 1) if i % x == 0]:
             continue
         ans.append(i)
     return ans
 
 
+# 0.182сек - результат вызова функции 1000 раз
+print(timeit.timeit("prime_list_2(100)", number=1000, setup="from __main__ import prime_list_2"))
+
+"""
+Третий вариант - поиск массива простых чисел через решето Эратосфена
+"""
+
+
 def resheto(num):
-    """
-    Решето Эратосфена. Возвращает список простых чисел до введенного.
-    :param num:
-    :return:
-    """
+    if num < 2:
+        return []
     ans = [x for x in range(2, num+1)]
     for i in ans:
         if i == 0:
@@ -49,13 +65,45 @@ def resheto(num):
             ans[ind] = 0
             ind += i
     return [x for x in ans if x != 0]
+    
+
+# 0.037сек - результат вызова функции 1000 раз
+print(timeit.timeit("resheto(100)", number=1000, setup="from __main__ import resheto"))
+
+"""
+В среднем, решето Эратосфена быстрее исходного алгоритма на порядок, 
+и более чем в четыре раза быстрее улучшенного исходного алгоритма
+"""
 
 
+def main():
+    n = 10000
+    prime_list(n)
+    prime_list_2(n)
+    resheto(n)
 
 
-if __name__ == '__main__':
-    print(timeit.timeit("prime_list(100)", number=1000, setup="from __main__ import prime_list"))
-    print(timeit.timeit("resheto(100)", number=1000, setup="from __main__ import resheto"))
-    # print(prime_list(40))
-    # print(resheto(40))
-# print(timeit.timeit("gen_list(20)", setup="from __main__ import gen_list"))
+cProfile.run('main()')
+
+"""
+22461 function calls in 4.808 seconds
+   ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+        1    0.038    0.038    4.748    4.748 2.py:15(prime_list) - Первый алгоритм
+     9998    4.710    0.000    4.710    0.000 2.py:18(<listcomp>)
+        1    0.011    0.011    0.053    0.053 2.py:34(prime_list_2) - Второй алгоритм
+     9998    0.042    0.000    0.042    0.000 2.py:37(<listcomp>)
+        1    0.005    0.005    0.006    0.006 2.py:51(resheto) - Решето Эратосфена
+        1    0.001    0.001    0.001    0.001 2.py:52(<listcomp>)
+        1    0.001    0.001    0.001    0.001 2.py:60(<listcomp>)
+        1    0.000    0.000    4.808    4.808 2.py:71(main)
+        1    0.000    0.000    4.808    4.808 <string>:1(<module>)
+        1    0.000    0.000    4.808    4.808 {built-in method builtins.exec}
+     2456    0.001    0.000    0.001    0.000 {method 'append' of 'list' objects}
+        1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+"""
+
+
+"""
+По итогам эмпирической оценки алгоритмов 
+можно с уверенностью сказать что Решето Эратосфена работает быстрее всего.
+"""
